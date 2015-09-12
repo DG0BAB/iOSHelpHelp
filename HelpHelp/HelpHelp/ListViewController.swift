d421f7b5a62a8d25199115e8fece24ea7296d7a5
Joachim Deelen <jdeelen@micabo.de>
2015-09-13 00:54:53 +0200

    Renamed

---
+++ HelpHelp/HelpHelp/ListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ListViewController.swift
+//  HelpHelp
+//
+//  Created by Joachim Deelen on 09.09.15.
+//  Copyright © 2015 micabo-software UG. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
